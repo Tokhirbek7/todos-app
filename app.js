2a6edf8a85c07d637f04224701edc98c3f4e2910
Tokhirbek <tokhirjonorifjonov2203@gmail.com>
2022-04-04 12:34:43 +0500

    minor update on post method

--- app.js
+++ app.js
@@ -1,15 +1,74 @@
+// thrid party libs
 const express = require('express')
+const { json } = require('express/lib/response')
+const res = require('express/lib/response')
 const app = express()
+
+// node libs
+const fs = require('fs')
 const PORT = 8000
 
 app.set('view engine', 'pug')
+app.use('/static', express('public'))
+app.use(express.urlencoded({extended:false}))
 
+// http://localhost:8000
 app.get('/', (req, res) =>{
-res.render('home')
+    fs.readFile('./data/todos.json', (err, data) => {
+        if(err) throw err
+
+        const todo = JSON.parse(data)
+        res.render('home', { todos: todo })
+        
+    })    
+
 });
 
-app.listen(PORT,  (err)=>{
+app.post('/add', (req, res) =>{ 
+    const formData = req.body
+})
+
+if( formData.todo.trim() == '' ){
+    fs.readFile('./data/todos.json', (err, data)=>{
+        if(err) throw err
+
+        const todos = JSON.parse(data)
+        res.render('home', { error:true, todo: todos })
+    })  
+}else{
+    fs.readFile('./data/todos.json', (err, data) => {
+        if(err) throw err
+
+        const todos = JSON.parse(data)
+
+        const todo = {
+            id: id(),
+            description: formData.todo,
+            done:false
+        }
+
+        todos.push(todo)
+
+        fs.writeFile('./data/todos.json',JSON.stringify(todos), (err) =>{
+            if (err) throw err
+            
+            fs.readFile('./data/todos.json', (err, data)=>{
+                if(err) throw err
+
+                const todos = JSON.parse(data)
+                res.render('home', { success:true, todo: todos })
+            })  
+            
+        } )
+    })
+}
+
+app.listen(PORT, (err)=>{
     if(err) throw err
 
-    console.log(`This app is running on port: ${PORT}`)
+    console.log(`This app is running on port: ${ PORT }`)
 });
+
+ function id () {
+    return '_' + Math.random().toString(36).substr(2, 9);
+  };
